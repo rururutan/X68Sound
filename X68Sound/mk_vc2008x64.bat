3b701bd11175136f51a9dfa8b41533d0474fb9fe
RuRuRu <rururutan@gmail.com>
2013-02-12 16:21:18 +0900

    Added /DC86CTL option.

--- X68Sound/mk_vc2008x64.bat
+++ X68Sound/mk_vc2008x64.bat
@@ -1,3 +1,3 @@
-cl /MT /EHsc /Ox /Ot /Oi /Ob2  /c  X68Sound.cpp
+cl /MT /EHsc /Ox /Ot /Oi /Ob2 /DC86CTL /c X68Sound.cpp
 rc x68sound.rc
 link /dll /def:x68sound.def x68sound.obj x68sound.res winmm.lib user32.lib
