6a69ecd32303c1f406c96ec16562073bd73ed69a
RuRuRu <rururutan+github@gmail.com>
2016-09-12 22:26:37 +0900

    Delete batch file for VC++6

--- X68Sound/mk_vc6.bat
+++
@@ -1,4 +0,0 @@
-cl /G6 /MT /GX /Ox /Ot /Oa /Og /Oi /Ob2  /c  JULIET.CPP
-cl /G6 /MT /GX /Ox /Ot /Oa /Og /Oi /Ob2  /c  X68Sound.cpp
-rc x68sound.rc
-link /dll /def:x68sound.def x68sound.obj juliet.obj x68sound.res winmm.lib user32.lib
